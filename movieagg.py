475304bc23c14ea7bc293d8930ecbcb580c3b263
cvartanian <64427702+cvartanian@users.noreply.github.com>
2021-11-26 21:04:28 -0500

    Initial Set Up

---
+++ movieagg.py
@@ -0,0 +1,48 @@
+import requests
+import re
+from sqlalchemy import create_engine
+import pandas as pd
+from sqlalchemy.sql.expression import false
+from bs4 import BeautifulSoup
+
+
+
+
+def get_genres():
+    r = requests.get("https://www.imdb.com/feature/genre?ref_=fn_asr_ge")
+    soup = BeautifulSoup(r.text,features="lxml")
+    genres = soup.find_all("div",{"class":"full-table"})[0].text.split('      ')[1:-1]
+        
+    return genres
+
+def get_movies(genre):
+    r = requests.get("https://www.imdb.com/search/title/?genres="+genre+"&explore=title_type,genres&view=simple")
+    
+    soup = BeautifulSoup(r.text,features="lxml")
+    links = soup.find_all("a", href=re.compile("/title/tt"),text=re.compile("^((?!\n).)*$"))
+    titles=[]
+    for each in links:
+        titles.append(each.text)
+
+    return titles
+
+
+if __name__ == "__main__":
+    genres = get_genres()
+    engine = create_engine('postgresql://chrisv:NWO@0.0.0.0:5432/movies')
+
+    top=[]
+    for genre in genres:
+        movies = get_movies(genre.replace(" ",""))
+        for index, movie in enumerate(movies):
+            top.append({"genre":genre,"movie":movie,"rank":index})
+    
+    df = pd.DataFrame(top)
+    df.to_sql('movies_fix', engine,index=false)
+    print("Written to DB")
+
+    print("Retrieving from DB")
+    print(pd.read_sql_table('movies',con=engine))
+
+
+    
